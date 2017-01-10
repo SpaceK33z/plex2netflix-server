5e2ef0f2709e9a7f48afa0e70cbcc90a7fd88e52
Kees Kluskens <kees@webduck.nl>
2017-01-10 22:10:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# plex2netflix Server
+
+This is the server part for [plex2netflix](https://github.com/SpaceK33z/plex2netflix).
+
+It does one simple thing: a request to an external API to check if an IMDb ID or movie title+year is available on Netflix. It then caches the result of the external API for a while, to prevent hammering it.
+
+The external API is graciously provided by [**uNoGS**](http://unogs.com/).
---
+++ server.js
@@ -0,0 +1,61 @@
+const dotenv = require('dotenv');
+const restify = require('restify');
+
+dotenv.config();
+
+const unogsApi = require('./lib/unogs');
+const addUsage = require('./lib/usage');
+
+const server = restify.createServer();
+server.use(restify.queryParser());
+
+const getUnixTimestamp = () => Math.floor(Date.now() / 1000);
+const cache = new Map();
+
+// We keep things in cache for 1 week.
+const cacheForSeconds = 7 * 24 * 60 * 60;
+function isFreshInCache(timestamp) {
+    const currentTimestamp = getUnixTimestamp();
+    return (currentTimestamp - timestamp) < cacheForSeconds;
+}
+
+server.get('/search', (req, res) => {
+    const imdb = req.query.imdb;
+
+    if (!imdb) {
+        res.send(400, { error: 'Add a `imdb` query parameter' });
+    }
+
+    function sendRes(data) {
+        res.send({
+            countries: data.countries,
+            checkedAt: data.checkedAt,
+        });
+    }
+
+    // Utilize cache instead of a unogs API request if it's still fresh
+    const cacheReq = cache.get(imdb);
+    if (cacheReq && isFreshInCache(cacheReq.checkedAt)) {
+        sendRes(cacheReq);
+        addUsage({ cache: true });
+        return;
+    }
+
+    unogsApi(imdb)
+    .then((countryList) => {
+        const data = {
+            countries: countryList,
+            checkedAt: getUnixTimestamp(),
+        };
+        cache.set(imdb, data);
+        addUsage({ cache: false });
+        sendRes(data);
+    })
+    .catch((err) => {
+        console.log('API error', err && err.response);
+    });
+});
+
+server.listen(8080, () => {
+    console.log(`Listening at ${server.url}`);
+});
